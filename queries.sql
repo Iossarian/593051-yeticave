a903595cecf2c4f58019f3c161f5f48447b19a9e
Iossarian <Kleymenov68@yandex.ru>
2018-09-27 12:54:08 +0300

    Исправление запросов

--- queries.sql
+++ queries.sql
@@ -17,24 +17,24 @@ INSERT INTO `users` (`id`, `reg_date`, `email`, `name`, `password`, `profile_img
 
 -- Заполнение таблицы "lots"
 INSERT INTO `lots` (`id`, `create_date`, `name`, `description`, `image`, `start_price`, `end_time`, `bet_step`, `author_id`, `winner_id`, `category_id`) VALUES
-(13, NULL, '2014 Rossignol District Snowboard', NULL, 'img/lot-1.jpg', 10999, NULL, NULL, 1, NULL, 1),
-(14, NULL, 'DC Ply Mens 2016/2017 Snowboard', NULL, 'img/lot-2.jpg', 159999, NULL, NULL, 5, NULL, 1),
-(15, NULL, 'Крепления Union Contact Pro 2015 года размер L/XL', NULL, 'img/lot-3.jpg', 8000, NULL, NULL, 5, NULL, 2),
-(16, NULL, 'Ботинки для сноуборда DC Mutiny Charocal', NULL, 'img/lot-4.jpg', 10999, NULL, NULL, 6, NULL, 3),
-(17, NULL, 'Куртка для сноуборда DC Mutiny Charocal', NULL, 'img/lot-5.jpg', 7500, NULL, NULL, 5, NULL, 4),
-(18, NULL, 'Маска Oakley Canopy', NULL, 'img/lot-6.jpg', 5400, NULL, NULL, 6, NULL, 6);
+(13, '2018-09-25 16:44:00', '2014 Rossignol District Snowboard', NULL, 'img/lot-1.jpg', 10999, '2018-09-29 16:44:00', NULL, 1, NULL, 1),
+(14, '2018-09-26 18:43:00', 'DC Ply Mens 2016/2017 Snowboard', NULL, 'img/lot-2.jpg', 159999, '2018-09-29 12:44:00', NULL, 5, NULL, 1),
+(15, '2018-09-25 12:03:00', 'Крепления Union Contact Pro 2015 года размер L/XL', NULL, 'img/lot-3.jpg', 8000, '2018-09-29 17:44:00', NULL, 5, NULL, 2),
+(16, '2018-09-24 17:04:00', 'Ботинки для сноуборда DC Mutiny Charocal', NULL, 'img/lot-4.jpg', 10999, '2018-09-27 16:44:00', NULL, 6, NULL, 3),
+(17, '2018-09-25 09:42:00', 'Куртка для сноуборда DC Mutiny Charocal', NULL, 'img/lot-5.jpg', 7500, '2018-09-29 20:44:00', NULL, 5, NULL, 4),
+(18, '2018-09-24 20:32:00', 'Маска Oakley Canopy', NULL, 'img/lot-6.jpg', 5400, '2018-09-28 21:42:00', NULL, 6, NULL, 6);
 
 -- Заполнение таблицы "bet"
 INSERT INTO `bet` (`id`, `date`, `price`, `lot_id`, `user_id`) VALUES
 (1, NULL, 8300, 15, 1),
-(2, NULL, 8000, 17, 5)
+(2, NULL, 8000, 17, 5),
 (3, NULL, 8500, 15, 5);
 
 -- Получение категорий
 SELECT * FROM category;
 
 -- Получение данных из таблицы 'lots'
-SELECT l.name, l.start_price, l.image, c.category_name, MAX(b.price), COUNT(b.id)
+SELECT l.name, l.start_price, l.image, c.category_name, l.create_date, MAX(b.price), COUNT(b.id)
 FROM 
 lots l 
 LEFT JOIN 
@@ -43,7 +43,9 @@ LEFT JOIN
 bet b ON b.lot_id = l.id 
 WHERE l.end_time > NOW() 
 GROUP BY 
-l.id DESC;
+l.id 
+ORDER BY l.create_date
+DESC;
 
 -- Показ лота по его Id и получение названия категории
 SELECT l.id, l.name, c.category_name AS category_name FROM lots AS l 
