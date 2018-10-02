beafba92c46aaa6870a36ed9e5d994f7a85a461c
Iossarian <Kleymenov68@yandex.ru>
2018-10-02 19:04:29 +0300

    работа с параметрами запроса

--- lot.php
+++ lot.php
@@ -17,14 +17,16 @@ $category_array = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
 //Подключение лотов
 $sql_lots = "SELECT lots.id, name, image, start_price, end_time, category_name FROM lots
             JOIN category ON category.id = lots.category_id
-            ORDER BY create_date DESC
-            WHERE lots.id = " .$id;
+            ORDER BY create_date DESC";
 $sql_lots_result = mysqli_query($con, $sql_lots);
 $lots_array = mysqli_fetch_all($sql_lots_result, MYSQLI_ASSOC);
 
 //Подключение параметра запроса
 $id = intval($_GET['id']);
-if ($result = mysqli_query($con, $sql_lots)) {
+$sql_l =    "SELECT lots.id, name, image, start_price, end_time, category_name FROM lots "
+            . "JOIN category ON category.id = lots.category_id "
+            . "WHERE lots.id = " .$id;
+if ($result = mysqli_query($con, $sql_l)) {
     if (!mysqli_num_rows($result)) {
         http_response_code(404);
     } else {
